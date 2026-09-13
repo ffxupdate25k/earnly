db59e2a33f8241475bdc9c0db0783e34bb74a57f
ffxupdate25k <ffxupdate25k@gmail.com>
2026-09-13 12:18:32 +0100

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,14 @@
+const user=JSON.parse(localStorage.getItem("earnly_user"));
 
+if(!user){
+location.href="login.html";
+}
+
+document.getElementById("name").innerText=user.fullname;
+document.getElementById("currency").innerText=user.currency;
+document.getElementById("balance").innerText=
+user.currency+" "+Number(user.balance).toFixed(2);
+
+if(user.email==="azeemolajuwon25@gmail.com"){
+document.getElementById("adminBtn").style.display="block";
+}
