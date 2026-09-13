bddaf51455f9b78bd1f50cc701c5de301cb9c70b
ffxupdate25k <ffxupdate25k@gmail.com>
2026-09-13 12:13:16 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,8 @@
 const SUPABASE_URL = "https://ponzxsvzmvbvhwuddxeo.supabase.co";
 
-const SUPABASE_KEY = "YOUR_ANON_KEY";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBvbnp4c3Z6bXZidmh3dWRkeGVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkyNTYzNDYsImV4cCI6MjEwNDgzMjM0Nn0.RYYVKNfjuYiDXKuhvw_NmHurEP5wvVCVz6iDgnGbPpw
+
+";
 
 const db = supabase.createClient(
   SUPABASE_URL,
